79e35d954c7efdf1baf314f53d5a8002c0a4f9d2
Keith Refson <Keith.Refson@stfc.ac.uk>
1990-08-29 18:11:45 +0000

    New option -b added for binary output. Allowed canonical+range specification of dump files.

--- src/dumpext.c
+++ src/dumpext.c
@@ -2,6 +2,8 @@
 #include 	<stdio.h>
 #include	"structs.h"
 
+char	*calloc();
+
 typedef struct list_t
 {
    struct list_t	*next;
@@ -73,19 +75,68 @@ int	*start, *finish, *inc;
  limerr:
    return -1;
 }
+/******************************************************************************
+ * Seq gen  Generate a sequence of file names from a "printf" style spec and  *
+ * range of filenames.							      *
+ ******************************************************************************/
+char **seqgen(fmt, range)
+char	*fmt, *range;
+{
+   int	start, finish, inc;
+   int  i, iseq = 0;
+   char **seq;
+   char	buf[256];
 
+   if( forstr(range, &start, &finish, &inc) != 0 )
+      return NULL;
+
+   if( (seq=(char**)calloc(1+(finish-start+1)/inc,sizeof(char*))) == NULL)
+      return NULL;
+
+   for(i = start; i <= finish; i++)
+   {
+      seq[iseq++] = strdup((sprintf(buf,fmt,i),buf));
+   }
+   seq[iseq] = NULL;
+   return seq;
+}
+/******************************************************************************
+ * Put.  Write data in text or binary form.				      *
+ ******************************************************************************/
 void
-extract(dump_name, pflg, rlist, qlist)
+put(buf, n, bflg)
+float *buf;
+int   n;
+int   bflg;
+{
+   char outbuf[32];
+   char *gcvt();
+   if( bflg )
+      fwrite((char*)buf, sizeof(float), n, stdout);
+   else
+      while(n-- > 0)
+      {
+	 fputs(gcvt(*buf++, 7, outbuf), stdout);
+	 putchar(' ');
+      }
+
+}
+/******************************************************************************
+ * Extract.  Process one dump file, extracting and outputting data.	      *
+ ******************************************************************************/
+void
+extract(dump_name, pflg, rlist, qlist, bflg)
 char	*dump_name;
 int	pflg;
 list_t	*rlist, *qlist;
+int	bflg;
 {
    FILE		*dump_file;
    dump_t	header;
    list_t	*r_ptr, *q_ptr;
    float	r_buf[3], q_buf[3];
-   int		idump, dump_base;
-   int		q_offset = 3 * sizeof(float) * nmols,
+   long		idump, dump_base;
+   long		q_offset = 3 * sizeof(float) * nmols,
    		h_offset = q_offset + 4 * sizeof(float) * nmols_r,
    		p_offset = h_offset + 9 * sizeof(float);
    
@@ -107,23 +158,23 @@ list_t	*rlist, *qlist;
       if(pflg)
       {
 	 fseek(dump_file, dump_base + p_offset, 0);
-	 fread(&r_buf, sizeof(float), 1, dump_file);
-	 printf("%12.7g ", r_buf[0]);
+	 fread((char*)r_buf, sizeof(float), 1, dump_file);
+	 put(r_buf, 1, bflg);
       }
       for(r_ptr = rlist; r_ptr; r_ptr = r_ptr->next)
       {
 	 fseek(dump_file, dump_base + 3*sizeof(float)*r_ptr->i, 0);
-	 fread(&r_buf, sizeof(float), 3, dump_file);
-	 printf("%12.7g %12.7g %12.7g", r_buf[0], r_buf[1], r_buf[2]);
+	 fread((char*)r_buf, sizeof(float), 3, dump_file);
+	 put(r_buf, 3, bflg);
       }
       for(q_ptr = qlist; q_ptr; q_ptr = q_ptr->next)
       {
 	 fseek(dump_file, dump_base + q_offset + 4*sizeof(float)*q_ptr->i, 0);
-	 fread(&q_buf, sizeof(float), 4, dump_file);
-	 printf("%12.7g %12.7g %12.7g %12.7g", q_buf[0], q_buf[1],
-		                               q_buf[2], q_buf[3]);
+	 fread((char*)q_buf, sizeof(float), 4, dump_file);
+	 put(q_buf, 4, bflg);
       }
-      putchar('\n');
+      if( ! bflg )
+	 putchar('\n');
       dump_base += sizeof(float) * header.dump_size;
    }
 }
@@ -132,14 +183,17 @@ main(argc, argv)
 int	argc;
 char	*argv[];
 {
-   int	c, val;
+   int	c;
    extern int	optind;
    extern char	*optarg;
-   int		errflg = 0;
+   int		errflg = 0, genflg = 0;
    char		*dump_name;
+   char		*range;
+   char		**filelist;
    FILE		*dump_file;
-   int		nfiles = 0, pflg = 0;
+   int		nfiles = 0, pflg = 0, bflg = 0;
    int		start,finish,inc, i;
+   int		find;
 
    dump_t	proto_header, header;
 
@@ -148,17 +202,24 @@ char	*argv[];
 
    r_head.next = q_head.next = f_head.next = NULL;
 
-   while( (c = getopt(argc, argv, "pR:r:Q:q:") ) != EOF )
+   while( (c = getopt(argc, argv, "pR:r:Q:q:n:b") ) != EOF )
       switch(c)
       {
        case 'p':
 	 pflg++;
+	 break;
+       case 'b':
+	 bflg++;
 	 break;
        case 'R':
 	 nmols = atoi(optarg);
 	 break;
        case 'Q':
 	 nmols_r = atoi(optarg);
+	 break;
+       case 'n':
+	 range = optarg;
+	 genflg++;
 	 break;
        case 'r':
 	 if( forstr(optarg, &start, &finish, &inc) )
@@ -196,17 +257,32 @@ char	*argv[];
 	 errflg++;
       }
 
+   if(optind >= argc)
+   {
+      fprintf(stderr,"%s: no dump files given\n",argv[0]);
+      errflg++;
+   }
+   if(genflg)
+   {
+      if( (filelist = seqgen(argv[optind], range)) == NULL)
+      {
+	 fprintf(stderr,"%s: invalid dump range \"%s\"\n", argv[0]);
+	 errflg++;
+      }
+   }
+   else
+      filelist = argv+optind;
+
    if( errflg )
    {
       fprintf(stderr,
-	      "Usage: dumpextract [-Rn] [-ri] [-Qn] [-qi] [-p] dumpfiles\n");
+	   "Usage: dumpextract [-Rn] [-ri] [-Qn] [-qi] [-p] [-n range] dumpfiles\n");
       exit(2);
    }
 
-   while( optind < argc )
+   for(find = 0; filelist[find] != NULL; find++)
    {
-      dump_name = argv[optind];
-      optind++;
+      dump_name = filelist[find];
       if( (dump_file = fopen(dump_name, "rb")) == NULL)
       {
 	 fprintf(stderr, "Failed to open dump file \"%s\"\n", dump_name);
@@ -242,6 +318,7 @@ char	*argv[];
    
    for( cur = f_head.next; cur; cur = cur->next)
    {
-      extract(cur->p, pflg, r_head.next, q_head.next);
+      extract(cur->p, pflg, r_head.next, q_head.next, bflg);
    }
+   return 0;
 }
