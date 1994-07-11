83abc070a78aac3f0306350ad35f09b2f198dfd6
Keith Refson <Keith.Refson@stfc.ac.uk>
1994-07-11 11:15:30 +0000

    Tidied up startup routine with par_broadcast() function. Documented parallel routine interface calls for porting.

--- src/parallel.c
+++ src/parallel.c
@@ -22,6 +22,9 @@ what you give them.   Help stamp out software-hoarding!  */
  * Parallel - support and interface routines to parallel MP libraries.	      *
  ******************************************************************************
  *       $Log: parallel.c,v $
+ * Revision 2.8  1994/07/07  17:00:26  keith
+ * Interface to BSP, TCGMSG and MPI message-passing libraries.
+ *
  * Revision 1.1  1994/07/07  16:59:44  keith
  * Initial revision
  *
@@ -49,30 +52,6 @@ void		init_averages();
 void		allocate_dynamics();
 extern int 	ithread, nthreads;
 /*====================== Utilities for interface functions ===================*/
-#ifdef TCGMSG
-/*
- * This gets called a lot.  A "prettier" C interface to BRDCST_.
- */
-cbrdcst(type, buf, lenbuf, ifrom)
-long 	type;
-gptr	*buf;
-long	lenbuf,
-	ifrom;
-{
-   switch (type & ~(long)0x7FFF)
-   {
-   case MSGINT: lenbuf *= sizeof(int);
-      break;
-   case MSGCHR: lenbuf *= sizeof(char);
-      break;
-   case MSGDBL: lenbuf *= sizeof(double);
-      break;
-   default:
-      break;
-   }
-   BRDCST_(&type, buf, &lenbuf, &ifrom);
-}
-#endif
 #ifdef BSP
 /*
  * BSP currently doesn't handle auto & heap vars.  This interface
@@ -80,22 +59,33 @@ long	lenbuf,
  */
 #define NBUFMAX 1048576
 static char tmpbuf[NBUFMAX];
-
-cbrdcst(ifrom, inbuf, outbuf, size)
-int	ifrom;
-char	*inbuf, *outbuf;
-int	size;
-{
-   if( size > NBUFMAX)
-      message(NULLI, NULLP, FATAL, 
-	      "cbrdcst called with too large n (%d) - maximum is %d",
-	      size, NBUFMAX);
-   memcp(tmpbuf, inbuf, size);
-   bspbroadcast(ifrom, tmpbuf, tmpbuf, size);
-   memcp(outbuf, tmpbuf, size);
-}
 #endif
-/*====================== Parallel lib interface functions ====================*/
+/*====================== Parallel lib interface functions ====================*
+ *  The following set of functions define the interface between moldy and     *
+ *  a message-passing parallel library.  To port to a new library it suffices *
+ *  to implement these in terms of the library primitives.  Thus all of the   *
+ *  implementation-dependent parallel code is confined to this file.          *
+ *									      *
+ *  The functions required are:						      *
+ *  par_sigintreset()	:  Moldy sets a handler for SIGINT.  This fn is called*
+ *			   from the signal handler to restore the default.    *
+ *  par_begin(int *argc, char ***argv, int *ithread, int *nthreads) 	      *
+ *                      :  Initialize the library and return the number of    *
+ *			   processes and the ID of this process.	      *
+ *  par_finish()	:  Terminate the parallel run normally.		      *
+ *  par_abort(int code) :  Terminate the run abnormally.  Return code if poss.*
+ *  par_broadcast(void *buf, int n, size_mt size, int ifrom)		      *
+ *			:  Broadcast the specified buffer from ifrom to all.  *
+ *  par_{r,d}sum(void *buf, int n) :  Perform a global parallel sum reduction *
+ *			   on the buffer containing n {reals,doubles}.	      *
+ *  par_imax(int *idat) :  Perform a global "maximum" reduction on the single *
+ *       		   int argument.				      *
+ *									      *
+ *  Note that there is no provision for heterogeneous execution by way of     *
+ *  type identification.  Though some MP libraries (eg MPI) do provide the    *
+ *  hooks it is too hard to implement for the control and other structs.      *
+ *  It's also hard to see why this might ever be useful for a MD run.	      *
+ *============================================================================*/
 /******************************************************************************
  * par_sigintreset().  Reset signal handler to parallel lib default upon trap *
  *		       of SIGINT.					      *
@@ -103,18 +93,21 @@ int	size;
 #ifdef TCGMSG
 extern	void    SigintHandler();
 
+void
 par_sigintreset()
 {
    signal(SIGINT, SigintHandler);
 }
 #endif
 #ifdef BSP
+void
 par_sigintreset()
 {
    signal(SIGINT, SIG_DFL);
 }
 #endif
 #ifdef MPI
+void
 par_sigintreset()
 {
    signal(SIGINT, SIG_DFL);
@@ -124,6 +117,7 @@ par_sigintreset()
  * par_imax().  Calculate global maximum over all processors.		      *
  ******************************************************************************/
 #ifdef TCGMSG
+void
 par_imax(idat)
 int *idat;
 {
@@ -138,6 +132,7 @@ int	size;
   *i1 = MAX(*i2,*i3);
 }
 
+void
 par_imax(idat)
 int *idat;
 {
@@ -145,6 +140,7 @@ int *idat;
 }
 #endif
 #ifdef MPI
+void
 par_imax(idat)
 int *idat;
 {
@@ -157,12 +153,14 @@ int *idat;
  * par_rsum()/dsum.  Calculate sum of "reals"/doubles  over all processors.   *
  ******************************************************************************/
 #ifdef TCGMSG
+void
 par_rsum(buf, n)
 real *buf;
 int  n;
 {
    DGOP_(ADDR(MSGDBL), buf, &n, "+");
 }
+void
 par_dsum(buf, n)
 real *buf;
 int  n;
@@ -188,6 +186,7 @@ int nb;
       res[i] = x[i] + y[i];
 }
 
+void
 par_rsum(buf, n)
 real *buf;
 int  n;
@@ -200,6 +199,7 @@ int  n;
    bspreduce(vradd, tmpbuf, tmpbuf, n*sizeof(real));
    memcp(buf, tmpbuf, n*sizeof(real));
 }
+void
 par_dsum(buf, n)
 double *buf;
 int  n;
@@ -218,6 +218,7 @@ int  n;
  * MPI demands seperate send and receive buffers.  Malloc one and keep
  * it around.  Extend if necessary.
  */
+void
 par_rsum(buf, n)
 real *buf;
 int  n;
@@ -234,6 +235,7 @@ int  n;
    MPI_Allreduce(buf, tmpbuf, n, M_REAL, MPI_SUM, MPI_COMM_WORLD);
    memcp(buf, tmpbuf, n*sizeof(real));
 }
+void
 par_dsum(buf, n)
 double *buf;
 int  n;
@@ -252,9 +254,55 @@ int  n;
 }
 #endif
 /******************************************************************************
+ * par_broadcast(). Broadcast data to all processors.			      *
+ ******************************************************************************/
+#ifdef TCGMSG
+void
+par_broadcast(buf, n, size, ifrom)
+gptr	*buf;
+int	n;
+size_mt	size;
+int	ifrom;
+{
+   long type = 0;
+   long	lenbuf = n*size;
+   long ifrm = ifrom;
+   BRDCST_(&type, buf, &lenbuf, &ifrm);
+}
+#endif
+#ifdef BSP
+void
+par_broadcast(buf, n, size, ifrom)
+gptr	*buf;
+int	n;
+size_mt	size;
+int	ifrom;
+{
+   if( n*size > NBUFMAX)
+      message(NULLI, NULLP, FATAL, 
+	      "cbrdcst called with too large n (%d) - maximum is %d",
+	      n*size, NBUFMAX);
+   memcp(tmpbuf, buf, n*size);
+   bspbroadcast(ifrom, tmpbuf, tmpbuf, n*size);
+   memcp(buf, tmpbuf, n*size);
+}
+#endif
+#ifdef MPI
+void
+par_broadcast(buf, n, size, ifrom)
+gptr	*buf;
+int	n;
+size_mt	size;
+int	ifrom;
+{
+   MPI_Bcast(buf, n*size, MPI_BYTE, ifrom, MPI_COMM_WORLD);
+}
+#endif
+/******************************************************************************
  * par_begin().  Initialize parallel libs.				      *
  ******************************************************************************/
 #ifdef TCGMSG
+void
 par_begin(argc, argv, ithread, nthreads)
 int	*argc;
 char	***argv;
@@ -271,6 +319,7 @@ int	*nthreads;
 }
 #endif
 #ifdef BSP
+void
 par_begin(argc, argv, ithread, nthreads)
 int	*argc;
 char	***argv;
@@ -281,6 +330,7 @@ int	*nthreads;
 }
 #endif
 #ifdef MPI
+void
 par_begin(argc, argv, ithread, nthreads)
 int	*argc;
 char	***argv;
@@ -296,18 +346,21 @@ int	*nthreads;
  * par_finish().  Parallel lib wind-up function.			      *
  ******************************************************************************/
 #ifdef TCGMSG
+void
 par_finish()
 {
    PEND_();
 }
 #endif
 #ifdef BSP
+void
 par_finish()
 {
    bspfinish();
 }
 #endif
 #ifdef MPI
+void
 par_finish()
 {
    MPI_Finalize();
@@ -317,6 +370,7 @@ par_finish()
  * par_abort().  Parallel lib abort function.				      *
  ******************************************************************************/
 #ifdef TCGMSG
+void
 par_abort(code)
 int code;
 {
@@ -324,6 +378,7 @@ int code;
 }
 #endif
 #ifdef BSP
+void
 par_abort(code)
 int code;
 {
@@ -333,6 +388,7 @@ int code;
 }
 #endif
 #ifdef MPI
+void
 par_abort(code)
 int code;
 {
@@ -350,29 +406,11 @@ pot_mp		*pot_ptr;		/* To be pointed at potpar array      */
 {
    spec_mp	spec;
    int		n_pot_recs;
-
+#ifdef SPMD
    /*
     * Fetch "system" struct
     */
-#ifdef BSP
-   cbrdcst(0, (gptr*)system, (gptr*)system, lsizeof(system_mt));
-#endif
-   /*
-    * N.B.  TCGMSG does not support transfer of structs using xdr.
-    * Therefore this code will only work between homogeneous nodes.
-    * (But that's all BSP does anyway)
-    */
-#ifdef TCGMSG
-   cbrdcst(0, (gptr*)system, lsizeof(system_mt), 0);
-#endif
-   /*
-    * MPI has facilities to describe struct types and transfer.
-    * Until someone gets around to actually doing it, this will
-    * work on homogeneous processors.
-    */
-#ifdef MPI
-   MPI_Bcast((gptr*)system, lsizeof(system_mt), MPI_BYTE, 0, MPI_COMM_WORLD);
-#endif
+   par_broadcast((gptr*)system, 1, lsizeof(system_mt), 0);
    /* Allocate space for species, site_info and potpar arrays and set pointers*/
    if( ithread > 0 )
    {
@@ -381,17 +419,7 @@ pot_mp		*pot_ptr;		/* To be pointed at potpar array      */
       *pot_ptr   = aalloc(system->max_id * system->max_id, pot_mt );
    }
    /*  read species array into allocated space				      */
-#ifdef BSP
-   cbrdcst(0,(gptr*)*spec_ptr,(gptr*)*spec_ptr, 
-	      system->nspecies*lsizeof(spec_mt));
-#endif
-#ifdef TCGMSG
-   cbrdcst(0, (gptr*)*spec_ptr, system->nspecies*lsizeof(spec_mt),0);
-#endif
-#ifdef MPI
-   MPI_Bcast((gptr*)*spec_ptr, system->nspecies*lsizeof(spec_mt), 
-	     MPI_BYTE, 0, MPI_COMM_WORLD);
-#endif
+   par_broadcast((gptr*)*spec_ptr, system->nspecies, lsizeof(spec_mt),0);
    /* Fill p_f_sites and site_id arrays for each species */
    for (spec = *spec_ptr; spec < &(*spec_ptr)[system->nspecies]; spec++)
    {
@@ -400,50 +428,18 @@ pot_mp		*pot_ptr;		/* To be pointed at potpar array      */
 	 spec->p_f_sites = ralloc(spec->nsites);  /* Allocate the species -     */
 	 spec->site_id   = ialloc(spec->nsites);  /* specific arrays	      */
       }
-#ifdef BSP
-      cbrdcst(0,(gptr*)spec->p_f_sites,(gptr*)spec->p_f_sites, 
-		 3*spec->nsites*lsizeof(real));
-      cbrdcst(0,(gptr*)spec->site_id,(gptr*)spec->site_id,   
-		 spec->nsites*lsizeof(int));
-#endif
-#ifdef TCGMSG
-      cbrdcst(MSGDBL, (gptr*)spec->p_f_sites, 3*spec->nsites, 0);
-      cbrdcst(MSGINT, (gptr*)spec->site_id,     spec->nsites, 0);
-#endif
-#ifdef MPI
-      MPI_Bcast((gptr*)spec->p_f_sites, 3*spec->nsites, M_REAL, 
-		0, MPI_COMM_WORLD);
-      MPI_Bcast((gptr*)spec->site_id, spec->nsites, MPI_INT, 
-		0, MPI_COMM_WORLD);
-#endif
+      par_broadcast((gptr*)spec->p_f_sites, 3*spec->nsites, sizeof(real), 0);
+      par_broadcast((gptr*)spec->site_id,     spec->nsites, sizeof(int), 0);
    }
    /* Fill site_info array */
-#ifdef BSP
-   cbrdcst(0, (gptr*)*site_info, (gptr*)*site_info, 
-	      system->max_id*lsizeof(site_mt));
-#endif
-#ifdef TCGMSG
-   cbrdcst(0, (gptr*)*site_info, system->max_id*lsizeof(site_mt), 0);
-#endif
-#ifdef MPI
-   MPI_Bcast((gptr*)*site_info, system->max_id*lsizeof(site_mt), 
-	     MPI_BYTE, 0, MPI_COMM_WORLD);
-#endif
+   par_broadcast((gptr*)*site_info, system->max_id, sizeof(site_mt), 0);
    /*
     * Potential Parameters.
     */
    n_pot_recs = SQR(system->max_id);
    if( ithread > 0 )
       *pot_ptr = (pot_mt*)aalloc(n_pot_recs*sizeof(pot_mt), char);
-#ifdef BSP
-   cbrdcst(0, (gptr*)*pot_ptr, (gptr*)*pot_ptr, n_pot_recs*sizeof(pot_mt));
-#endif
-#ifdef TCGMSG
-   cbrdcst(0, (gptr*)*pot_ptr, n_pot_recs*sizeof(pot_mt), 0);
-#endif
-#ifdef MPI
-   MPI_Bcast((gptr*)*pot_ptr, n_pot_recs*sizeof(pot_mt), 
-	     MPI_BYTE, 0, MPI_COMM_WORLD);
+   par_broadcast((gptr*)*pot_ptr, n_pot_recs, sizeof(pot_mt), 0);
 #endif
 }
 /******************************************************************************
@@ -454,102 +450,31 @@ system_mp	system;
 {
    gptr		*ap;			/* Pointer to averages database       */
    size_mt	asize;			/* Size of averages database	      */
-
-#ifdef BSP
-   cbrdcst(0, (gptr*)system->c_of_m, (gptr*)system->c_of_m, 
-	      3*system->nmols*lsizeof(real));
-   cbrdcst(0, (gptr*)system->vel, (gptr*)system->vel,    
-	      3*system->nmols*lsizeof(real));
-   cbrdcst(0, (gptr*)system->velp, (gptr*)system->velp,   
-	      3*system->nmols*lsizeof(real));
-   cbrdcst(0, (gptr*)system->acc, (gptr*)system->acc,    
-	      3*system->nmols*lsizeof(real));
-   cbrdcst(0, (gptr*)system->acco, (gptr*)system->acco,   
-	      3*system->nmols*lsizeof(real));
-   cbrdcst(0, (gptr*)system->accvo, (gptr*)system->accvo,  
-	      3*system->nmols*lsizeof(real));
+#ifdef SPMD
+   par_broadcast((gptr*)system->c_of_m,3*system->nmols, sizeof(real), 0);
+   par_broadcast((gptr*)system->vel,   3*system->nmols, sizeof(real), 0);
+   par_broadcast((gptr*)system->velp,  3*system->nmols, sizeof(real), 0);
+   par_broadcast((gptr*)system->acc,   3*system->nmols, sizeof(real), 0);
+   par_broadcast((gptr*)system->acco,  3*system->nmols, sizeof(real), 0);
+   par_broadcast((gptr*)system->accvo, 3*system->nmols, sizeof(real), 0);
    if(system->nmols_r > 0)
    {
-      cbrdcst(0, (gptr*)system->quat, (gptr*)system->quat,    
-		 4*system->nmols_r*lsizeof(real));
-      cbrdcst(0, (gptr*)system->qdot, (gptr*)system->qdot,    
-		 4*system->nmols_r*lsizeof(real));
-      cbrdcst(0, (gptr*)system->qdotp, (gptr*)system->qdotp,   
-		 4*system->nmols_r*lsizeof(real));
-      cbrdcst(0, (gptr*)system->qddot, (gptr*)system->qddot,   
-		 4*system->nmols_r*lsizeof(real));
-      cbrdcst(0, (gptr*)system->qddoto, (gptr*)system->qddoto,  
-		 4*system->nmols_r*lsizeof(real));
-      cbrdcst(0, (gptr*)system->qddotvo, (gptr*)system->qddotvo, 
-		 4*system->nmols_r*lsizeof(real));
+      par_broadcast((gptr*)system->quat,    4*system->nmols_r, sizeof(real), 0);
+      par_broadcast((gptr*)system->qdot,    4*system->nmols_r, sizeof(real), 0);
+      par_broadcast((gptr*)system->qdotp,   4*system->nmols_r, sizeof(real), 0);
+      par_broadcast((gptr*)system->qddot,   4*system->nmols_r, sizeof(real), 0);
+      par_broadcast((gptr*)system->qddoto,  4*system->nmols_r, sizeof(real), 0);
+      par_broadcast((gptr*)system->qddotvo, 4*system->nmols_r, sizeof(real), 0);
    }
-   cbrdcst(0, (gptr*)system->h,      (gptr*)system->h,       9*lsizeof(real));
-   cbrdcst(0, (gptr*)system->hdot,   (gptr*)system->hdot,    9*lsizeof(real));
-   cbrdcst(0, (gptr*)system->hdotp,  (gptr*)system->hdotp,   9*lsizeof(real));
-   cbrdcst(0, (gptr*)system->hddot,  (gptr*)system->hddot,   9*lsizeof(real));
-   cbrdcst(0, (gptr*)system->hddoto, (gptr*)system->hddoto,  9*lsizeof(real));
-   cbrdcst(0, (gptr*)system->hddotvo,(gptr*)system->hddotvo, 9*lsizeof(real));
+   par_broadcast((gptr*)system->h,       9, sizeof(real), 0); 
+   par_broadcast((gptr*)system->hdot,    9, sizeof(real), 0);
+   par_broadcast((gptr*)system->hdotp,   9, sizeof(real), 0); 
+   par_broadcast((gptr*)system->hddot,   9, sizeof(real), 0);
+   par_broadcast((gptr*)system->hddoto,  9, sizeof(real), 0); 
+   par_broadcast((gptr*)system->hddotvo, 9, sizeof(real), 0);
 
    ap = av_ptr(&asize,0);	      /* get addr, size of database   */
-   cbrdcst(0,ap,ap, asize);
-#endif
-#ifdef TCGMSG
-   cbrdcst(MSGDBL, (gptr*)system->c_of_m,3*system->nmols, 0);
-   cbrdcst(MSGDBL, (gptr*)system->vel,   3*system->nmols, 0);
-   cbrdcst(MSGDBL, (gptr*)system->velp,  3*system->nmols, 0);
-   cbrdcst(MSGDBL, (gptr*)system->acc,   3*system->nmols, 0);
-   cbrdcst(MSGDBL, (gptr*)system->acco,  3*system->nmols, 0);
-   cbrdcst(MSGDBL, (gptr*)system->accvo, 3*system->nmols, 0);
-   if(system->nmols_r > 0)
-   {
-      cbrdcst(MSGDBL, (gptr*)system->quat,    4*system->nmols_r, 0);
-      cbrdcst(MSGDBL, (gptr*)system->qdot,    4*system->nmols_r, 0);
-      cbrdcst(MSGDBL, (gptr*)system->qdotp,   4*system->nmols_r, 0);
-      cbrdcst(MSGDBL, (gptr*)system->qddot,   4*system->nmols_r, 0);
-      cbrdcst(MSGDBL, (gptr*)system->qddoto,  4*system->nmols_r, 0);
-      cbrdcst(MSGDBL, (gptr*)system->qddotvo, 4*system->nmols_r, 0);
-   }
-   cbrdcst(MSGDBL, (gptr*)system->h,       9, 0); 
-   cbrdcst(MSGDBL, (gptr*)system->hdot,    9, 0);
-   cbrdcst(MSGDBL, (gptr*)system->hdotp,   9, 0); 
-   cbrdcst(MSGDBL, (gptr*)system->hddot,   9, 0);
-   cbrdcst(MSGDBL, (gptr*)system->hddoto,  9, 0); 
-   cbrdcst(MSGDBL, (gptr*)system->hddotvo, 9, 0);
-
-   ap = av_ptr(&asize,0);	      /* get addr, size of database   */
-   cbrdcst(0, ap, asize,0);
-#endif
-#ifdef MPI
-   MPI_Bcast((gptr*)system->c_of_m,3*system->nmols, M_REAL, 0, MPI_COMM_WORLD);
-   MPI_Bcast((gptr*)system->vel,   3*system->nmols, M_REAL, 0, MPI_COMM_WORLD);
-   MPI_Bcast((gptr*)system->velp,  3*system->nmols, M_REAL, 0, MPI_COMM_WORLD);
-   MPI_Bcast((gptr*)system->acc,   3*system->nmols, M_REAL, 0, MPI_COMM_WORLD);
-   MPI_Bcast((gptr*)system->acco,  3*system->nmols, M_REAL, 0, MPI_COMM_WORLD);
-   MPI_Bcast((gptr*)system->accvo, 3*system->nmols, M_REAL, 0, MPI_COMM_WORLD);
-   if(system->nmols_r > 0)
-   {
-      MPI_Bcast((gptr*)system->quat,    4*system->nmols_r, 
-		M_REAL, 0, MPI_COMM_WORLD);
-      MPI_Bcast((gptr*)system->qdot,    4*system->nmols_r, 
-		M_REAL, 0, MPI_COMM_WORLD);
-      MPI_Bcast((gptr*)system->qdotp,   4*system->nmols_r, 
-		M_REAL, 0, MPI_COMM_WORLD);
-      MPI_Bcast((gptr*)system->qddot,   4*system->nmols_r, 
-		M_REAL, 0, MPI_COMM_WORLD);
-      MPI_Bcast((gptr*)system->qddoto,  4*system->nmols_r, 
-		M_REAL, 0, MPI_COMM_WORLD);
-      MPI_Bcast((gptr*)system->qddotvo, 4*system->nmols_r, 
-		M_REAL, 0, MPI_COMM_WORLD);
-   }
-   MPI_Bcast((gptr*)system->h,       9, M_REAL, 0, MPI_COMM_WORLD); 
-   MPI_Bcast((gptr*)system->hdot,    9, M_REAL, 0, MPI_COMM_WORLD);
-   MPI_Bcast((gptr*)system->hdotp,   9, M_REAL, 0, MPI_COMM_WORLD); 
-   MPI_Bcast((gptr*)system->hddot,   9, M_REAL, 0, MPI_COMM_WORLD);
-   MPI_Bcast((gptr*)system->hddoto,  9, M_REAL, 0, MPI_COMM_WORLD); 
-   MPI_Bcast((gptr*)system->hddotvo, 9, M_REAL, 0, MPI_COMM_WORLD);
-
-   ap = av_ptr(&asize,0);	      /* get addr, size of database   */
-   MPI_Bcast(ap, asize, MPI_BYTE, 0, MPI_COMM_WORLD);
+   par_broadcast(ap, 1, asize,0);
 #endif
 }
 /******************************************************************************
@@ -570,19 +495,9 @@ restrt_mt *restart_header;
    /*
     *  Fetch the top-level structs
     */
-#ifdef BSP
-   cbrdcst(0, control, control, sizeof *control);
-   cbrdcst(0, restart_header, restart_header, sizeof *restart_header);
-#endif
-#ifdef TCGMSG
-   cbrdcst(0, (gptr*)control, sizeof *control, 0);
-   cbrdcst(0, (gptr*)restart_header, sizeof *restart_header, 0);
-#endif
-#ifdef MPI
-   MPI_Bcast((gptr*)control, sizeof *control, MPI_BYTE, 0, MPI_COMM_WORLD);
-   MPI_Bcast((gptr*)restart_header, sizeof *restart_header, 
-	     MPI_BYTE, 0, MPI_COMM_WORLD);
-#endif
+#ifdef SPMD
+   par_broadcast((gptr*)control, 1, sizeof *control, 0);
+   par_broadcast((gptr*)restart_header, 1, sizeof *restart_header, 0);
    /*
     * Now get "species" struct array and internal dynamic arrays
     */
@@ -603,5 +518,6 @@ restrt_mt *restart_header;
     * Copy the dynamic vars from the other processes.
     */
    copy_dynamics(system);
+#endif
 }
 
