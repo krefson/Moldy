07b3acf24c64dade72d5a8deaec3095820e4af52
Keith Refson <Keith.Refson@stfc.ac.uk>
1995-12-04 11:51:32 +0000

    Nose-Hoover and Gaussian (Hoover constrained) thermostats added. Thanks to V. Murashov.

--- src/messages.h
+++ src/messages.h
@@ -19,7 +19,7 @@ In other words, you are welcome to use, share and improve this program.
 You are forbidden to forbid anyone else to use, share and improve
 what you give them.   Help stamp out software-hoarding!  */
 /*
- * $Header: /home/eeyore_data/keith/md/moldy/RCS/messages.h,v 2.8 1995/01/05 09:50:57 keith stab $
+ * $Header: /home/eeyore_data/keith/md/moldy/RCS/messages.h,v 2.8 1995/01/05 09:50:57 keith stab keith $
  */
 
 #ifndef SYSRD   /* Skip if already defined				      */
@@ -66,8 +66,7 @@ what you give them.   Help stamp out software-hoarding!  */
 #define	REREAD	"read error on restart file at byte %ld\n    -  %s"
 #define	REEOF	"unexpected end of file reading restart file"
 #define	REWRT	"write error on restart file\n    -  %s"
-#define	NOKEY	"no keyword found on line"
-#define	NOVAL	"no value associated with keyword \"%s\""
+#define	NOVAL	"no value associated with name \"%s\""
 #define	NOTFND	"keyword \"%s\" not found"
 #define	BADVAL	"value \"%s\" is wrong type for keyword \"%s\""
 #define	ERRCON	"control file contains %d error%c"
@@ -157,3 +156,6 @@ number of words"
 #define SYSEOF	"Unexpected end of file encountered while reading %s"
 #define MAXCUT  "Optimum cutoff of %2.2f A is larger than allowed maximum \
 of %2.2f.\n Maximum will be used but accuracy of Ewald sum is compromised."
+#define GANDP   "Application of Parinello-Rahman algorithm along with \
+Gaussian thermostat would interfere with each other."
+#define	NOKEY	"no keyword found on line"
