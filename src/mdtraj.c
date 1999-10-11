05c54965aec59421f7e67a17fab4ee18d09ce5c4
Keith Refson <Keith.Refson@stfc.ac.uk>
1999-10-11 14:31:52 +0000

    Incorporated site-pbc branch "bekker" into main "Beeman" branch.

--- src/mdtraj.c
+++ src/mdtraj.c
@@ -25,6 +25,9 @@ what you give them.   Help stamp out software-hoarding! */
  ************************************************************************************** 
  *  Revision Log
  *  $Log: mdtraj.c,v $
+ *  Revision 1.1  1998/01/09 11:35:11  keith
+ *  Initial revision
+ *
  *  Revision 1.3  1997/07/10 18:05:26  craig
  *  Previous slice spec_mt converted to cofm array
  *  Traj_cofm zeroed after call to arralloc
@@ -52,6 +55,7 @@ what you give them.   Help stamp out software-hoarding! */
 #include <stdio.h>
 #include "structs.h"
 #include "messages.h"
+#include "utlsup.h"
 #if defined(ANSI) || defined(__STDC__)
 gptr	*arralloc(size_mt,int,...); 	/* Array allocator		      */
 #else
@@ -72,400 +76,16 @@ void	read_restart();
 void	init_averages();
 int	getopt();
 gptr	*talloc();
-FILE	*popen();
+void	mat_vec_mul3();
+void    zero_real();
+void    tfree();
 /*======================== Global vars =======================================*/
 int ithread=0, nthreads=1;
-/******************************************************************************
- * Dummies of 'moldy' routines so that mdtraj may be linked with moldy library*
- ******************************************************************************/
-void 	init_rdf()
-{}
-gptr *rdf_ptr()
-{return 0;}
-void new_lins()
-{}
-int lines_left()
-{return 0;}
-void new_page()
-{}
-void	new_line()
-{
-   (void)putchar('\n');
-}
-void	banner_page()
-{}
-void	note()
-{}
-void	conv_potentials()
-{}
-void	conv_control()
-{}
-/******************************************************************************
- *  message.   Deliver error message to possibly exiting.  It can be called   *
- *             BEFORE output file is opened, in which case outt to stderr.    *
- ******************************************************************************/
-#if defined(ANSI) || defined(__STDC__)
-#   undef  va_alist
-#   define      va_alist int *nerrs, ...
-#   ifdef va_dcl
-#      undef va_dcl
-#   endif
-#   define va_dcl /* */
-#endif
-/*VARARGS*/
-void    message(va_alist)
-va_dcl
-{
-   va_list      ap;
-   char         *buff;
-   int          sev;
-   char         *format;
-   static char  *sev_txt[] = {" *I* "," *W* "," *E* "," *F* "};
-#if defined(ANSI) || defined(__STDC__)
-   va_start(ap, nerrs);
-#else
-   int          *nerrs;
-
-   va_start(ap);
-   nerrs = va_arg(ap, int *);
-#endif
-   buff  = va_arg(ap, char *);
-   sev   = va_arg(ap, int);
-   format= va_arg(ap, char *);
-
-   (void)fprintf(stderr, "mdtraj: ");
-   (void)vfprintf(stderr, format, ap);
-   va_end(ap);
-   fputc('\n',stderr);
-
-   if(buff != NULL)                     /* null ptr means don't print buffer  */
-   {
-      (void)fprintf(stderr,"     buffer contents=\"%s\"",buff);
-      fputc('\n',stderr);
-   }
-   if(sev >= ERROR && nerrs != NULL)
-      (*nerrs)++;
-   if(sev == FATAL)
-      exit(3);
-}
-
-/******************************************************************************
- *  message.   Deliver error message to possibly exiting. 		      *
- ******************************************************************************/
-#if defined(ANSI) || defined(__STDC__)
-#undef  va_alist
-#define	va_alist char *format, ...
-#ifdef  va_dcl
-#   undef  va_dcl
-#endif
-#define va_dcl /* */
-#endif
-/*VARARGS*/
-void	error(va_alist)
-va_dcl
-{
-   va_list	ap;
-#if defined(ANSI) || defined(__STDC__)
-   va_start(ap, format);
-#else
-   char		*format;
-
-   va_start(ap);
-   format= va_arg(ap, char *);
-#endif
-
-   (void)fprintf(stderr, "mdtraj: ");
-   (void)vfprintf(stderr, format, ap);
-   fputc('\n',stderr);
-   va_end(ap);
-
-   exit(3);
-}
-static char * mystrdup(s)
-char *s;
-{
-   char * t=malloc(strlen(s)+1);
-   return t?strcpy(t,s):0;
-}
-/******************************************************************************
- * get_int().  Read an integer from stdin, issuing a prompt and checking      *
- * validity and range.  Loop until satisfied, returning EOF if appropriate.   *
- ******************************************************************************/
-int get_int(prompt, lo, hi)
-char	*prompt;
-int	lo, hi;
-{
-   char		ans_str[80];
-   int		ans_i, ans_flag;
-
-   ans_flag = 0;
-   while( ! feof(stdin) && ! ans_flag )
-   {
-      fputs(prompt, stderr);
-      fflush(stderr);
-      fgets(ans_str, sizeof ans_str, stdin);
-      if( sscanf(ans_str, "%d", &ans_i) == 1 && ans_i >= lo && ans_i <= hi)
-	 ans_flag++;
-   }
-   if( ans_flag )
-      return(ans_i);
-   else
-      return(EOF);
-}
-/******************************************************************************
- * get_real().  Read an int from stdin, issuing a prompt and checking         *
- * validity and range.  Loop until satisfied, returning EOF if appropriate.   *
- ******************************************************************************/
-int get_real(prompt, lo, hi)
-char	*prompt;
-real 	lo, hi;
-{
-   char		ans_str[80];
-   int	ans_r; 
-   int		ans_flag;
-   ans_flag = 0;
-   while( ! feof(stdin) && ! ans_flag )
-   {
-      fputs(prompt, stderr);
-      fflush(stderr);
-      fgets(ans_str, sizeof ans_str, stdin);
-      if( sscanf(ans_str, "%d", &ans_r) == 1 && ans_r >= lo && ans_r <= hi)
-	 ans_flag++;
-   }
-   if( ans_flag )
-      return(ans_r);
-   else
-      return(EOF);
-}
-/******************************************************************************
- * get_sym().  Read a character from stdin and match to supplied set	      *
- ******************************************************************************/
-int get_sym(prompt, cset)
-char	*prompt;
-char	*cset;
-{
-   char		ans_c, ans_str[80];
-   int		ans_flag;
-
-   ans_flag = 0;
-   while( ! feof(stdin) && ! ans_flag )
-   {
-      fputs(prompt, stderr);
-      fflush(stderr);
-      fgets(ans_str, sizeof ans_str, stdin);
-      if( sscanf(ans_str, " %c", &ans_c) == 1 && strchr(cset, ans_c))
-	 ans_flag++;
-   }
-   if( ans_flag )
-      return(ans_c);
-   else
-      return(EOF);
-}
-/******************************************************************************
- * get_str().  Read a string from stdin, issuing a prompt.		      *
- ******************************************************************************/
-char	*get_str(prompt)
-char	*prompt;
-{
-   char		ans_str[80];
-   char		*str = malloc(80);
-   int		ans_flag;
-
-   ans_flag = 0;
-   while( ! feof(stdin) && ! ans_flag )
-   {
-      fputs(prompt, stderr);
-      fflush(stderr);
-      fgets(ans_str, sizeof ans_str, stdin);
-      if( sscanf(ans_str, "%s", str) == 1)
-	 ans_flag++;
-   }
-   if( ans_flag )
-      return(str);
-   else
-      return(NULL);
-}
-/******************************************************************************
- * forstr.  Parse string str of format s-f:n (final 2 parts optional),        *
- *          returning integer values of s,f,n. f defaults to s and n to 1     *
- ******************************************************************************/
-int
-forstr(instr, start, finish, inc)
-char	*instr;
-int	*start, *finish, *inc;
-{
-   char	*p, *pp, *str = mystrdup(instr);
-   long strtol();
-   
-   if( (p = strchr(str,':')) != NULL)
-   {
-      *inc = strtol(p+1, &pp, 0);
-      if( pp == p+1 )
-	 goto limerr;
-      *p = 0;
-   }
-   else
-      *inc = 1;
-   if( (p = strchr(str,'-')) != NULL)
-   {
-      *p = 0;
-      *start = strtol(str, &pp, 0);
-      if( pp == str )
-	 goto limerr;
-      *finish = strtol(p+1, &pp, 0);
-      if( pp == p+1 )
-	 goto limerr;
-   }
-   else
-   {
-      *start = *finish = strtol(str, &pp, 0);
-      if( pp == str )
-	 goto limerr;
-   }
-   if( *start+*inc > *finish+1 || *start < 0 || *inc <= 0 )
-   {
-      fputs("Limits must satisfy", stderr);
-      fputs(" finish >= start + increment, start >= 0 and increment > 0\n", stderr);
-      goto limerr;
-   }
-   return 0;
- limerr:
-   return -1;
-}
-/******************************************************************************
- * range_in. Function for determining ranges of atom positions to include     *
- ******************************************************************************/
-int
-range_in(system, range, range_flag)
-system_mt	*system;
-real		range[3][2];
-int		*range_flag;
-{
-   double	box[3];
-   mat_mp	h = system->h;
-   int		i;
-
-   for( i=0; i<3; i++)
-   {
-       box[i] = sqrt(SQR(h[0][i]) + SQR(h[1][i]) + SQR(h[2][i]));
-
-       if( range_flag[i])
-          switch(i)
-          {
-           case 0:
-             range[i][0] = get_real("Enter x minimum: ", -1*box[i],box[i]);
-             range[i][1] = get_real("Enter x maximum: ", range[i][0],2*box[i]);
-             break;
-           case 1:
-             range[i][0] = get_real("Enter y minimum: ", -1*box[i],box[i]);
-             range[i][1] = get_real("Enter y maximum: ", range[i][0],2*box[i]);
-             break;
-           case 2:
-             range[i][0] = get_real("Enter z minimum: ", -1*box[i],box[i]);
-             range[i][1] = get_real("Enter z maximum: ", range[i][0],2*box[i]);
-             break;
-          }    
-       else
-       {
-          range[i][0] = -1*box[i];
-          range[i][1] = box[i];
-       }
-   }
-   return 0;
-}
-/******************************************************************************
- * dump_to_moldy.  Fill the 'system' arrays with the dump data in 'buf' (see  *
- * dump.c for format), expanding floats to doubles if necessary.              *
- ******************************************************************************/
 #define DUMP_SIZE(level)  (( (level & 1) + (level>>1 & 1) + (level>>2 & 1) ) * \
 			            (3*sys.nmols + 4*sys.nmols_r + 9)+ \
 			     (level>>3 & 1) * \
 			            (3*sys.nmols + 3*sys.nmols_r + 9) +\
 			     (level & 1))
-void
-dump_to_moldy(buf, system)
-float	*buf;
-system_mt *system;
-{
-   int i;
-   float	*c_of_m = buf;
-   float	*quat   = buf+3*system->nmols;
-   float	*h      = buf+3*system->nmols + 4*system->nmols_r;
-   mat_mt	hinv;
-
-/* $dir no_recurrence */
-   for(i = 0; i < system->nmols; i++)
-   {
-      system->c_of_m[i][0] = c_of_m[3*i];
-      system->c_of_m[i][1] = c_of_m[3*i+1];
-      system->c_of_m[i][2] = c_of_m[3*i+2]; 
-   }
-/* $dir no_recurrence */
-   for(i = 0; i < system->nmols_r; i++)
-   {
-      system->quat[i][0] = quat[4*i];
-      system->quat[i][1] = quat[4*i+1];
-      system->quat[i][2] = quat[4*i+2];
-      system->quat[i][3] = quat[4*i+3];
-   }
-/* $dir no_recurrence */
-   for(i = 0; i < 3; i++)
-   {
-      system->h[i][0] = h[3*i];
-      system->h[i][1] = h[3*i+1];
-      system->h[i][2] = h[3*i+2];
-   }
-   invert(system->h,hinv);
-   mat_vec_mul(hinv, system->c_of_m, system->c_of_m, system->nmols);
-}
-/******************************************************************************
- ******************************************************************************/
-void mat_vec_mul3(m, vec, number)
-int             number;         /* Number of vectors to be multiplied         */
-real            m[3][3];        /* Matrix                                     */
-real            **vec;          /* Output vector.  CAN BE SAME AS INPUT  (out)*/
-{
-   int i;
-   register double        a0, a1, a2;
-
-   for(i = 0; i < number; i++)
-   {
-      a0 = vec[i][0];  a1 = vec[i][1];  a2 = vec[i][2];
-
-      vec[i][0] = m[0][0]*a0 + m[0][1]*a1 + m[0][2]*a2;
-      vec[i][1] = m[1][0]*a0 + m[1][1]*a1 + m[1][2]*a2;
-      vec[i][2] = m[2][0]*a0 + m[2][1]*a1 + m[2][2]*a2;
-   }
-}
-/******************************************************************************
- * traj_con().  Connect molecular c_of_m`s into continuous trajectories       * 
- ******************************************************************************/
-void
-traj_con(system, species, prev_cofm, traj_cofm, sp_range)
-system_mt	*system;
-spec_mt		species[];
-real		**prev_cofm,
-		**traj_cofm;
-int		sp_range[3];
-{
-   spec_mt	*spec;
-   int		i, imol, ispec, totmol=0;
- 
-   for( ispec = sp_range[0], spec=species+sp_range[0]; spec <= species+sp_range[1]; 
-                                              ispec+=sp_range[2], spec+=sp_range[2])
-     for( imol=0; imol<spec->nmols; totmol++, imol++)
-	if( prev_cofm == 0 ) 
-	{
-	   for( i = 0; i < 3; i++)
-	      traj_cofm[totmol][i] = spec->c_of_m[imol][i];
-	}
-	else 
-	{
-	   for( i = 0; i < 3; i++)
-	      traj_cofm[totmol][i] = spec->c_of_m[imol][i] - 
-		 floor(spec->c_of_m[imol][i]-prev_cofm[totmol][i]+0.5);
-	}
-}
 /******************************************************************************
  * traj_out().  Output routine for displaying trajectories                    * 
  ******************************************************************************/
@@ -545,8 +165,8 @@ char	*argv[];
    int		rflag, sflag;
    int		irec;
    char		*filename = NULL, *dump_name = NULL;
-   char		*dumplims = NULL, *atom_sel = NULL;
-   char		*insert = NULL, *speclims = NULL;
+   char		*dumplims = NULL;
+   char		*speclims = NULL;
    char		*tempname;
    char		dumpcommand[256];
    int		dump_size;
@@ -563,7 +183,6 @@ char	*argv[];
    pot_mt	*potpar;
    quat_mt	*qpf;
    contr_mt	control_junk;
-   int		i,j,it;
 
 #define MAXTRY 100
    control.page_length=1000000;
@@ -775,10 +394,10 @@ char	*argv[];
         if( irec == start )
 	{
           range_in(&sys, range, range_flag);
-          traj_con(&sys, species, 0, traj_cofm[islice], sp_range);
+          traj_con2(&sys, species, 0, traj_cofm[islice], sp_range);
 	}
 	else
-          traj_con(&sys, species, traj_cofm[islice-1], traj_cofm[islice], sp_range);
+          traj_con2(&sys, species, traj_cofm[islice-1], traj_cofm[islice], sp_range);
 
         islice++;
 
