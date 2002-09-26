d95d8161c20ed97ea8ab987275e4390322a7acce
Anonymous Developer <>
2002-09-26 10:13:39 +0000

    Extra definitions needed for "autoheader"

---
+++ src/acconfig.h
@@ -0,0 +1,22 @@
+/* Define to use BSP parallel interface */
+#undef BSP
+
+/* Define to use old BSP parallel interface */
+#undef BSP0
+
+/* Define if you have the <bsp.h> header file. */
+#undef HAVE_BSP_H
+
+/* Define if you have the <bsp_level1.h> header file. */
+#undef HAVE_BSP_LEVEL1_H
+
+/* Define to compile for parallelism */
+#undef SPMD
+
+/* Define to use TCGMSG parallel interface */
+#undef TCGMSG
+
+/* Define if the times system call returns a status flag instead of the RT
+   clock */
+#undef TIMES_RETURNS_STATUS
+
