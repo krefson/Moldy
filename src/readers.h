ee0f37eb9e183fbba0d830a4a8af381b9e6aca0a
Craig Fisher <c_fisher@jfcc.or.jp>
2004-12-06 19:11:36 +0000

    Declarations and definitions for functions in readers.c.

---
+++ src/readers.h
@@ -0,0 +1,14 @@
+/* reader functions */
+char	*read_ftype(char *filename);
+int     read_ele(spec_data *element, char *filename);
+int     read_pdb(char *, mat_mp, char (*)[NLEN], vec_mp, double *, char *, char *);
+int     read_cssr(char *, mat_mp, char (*)[NLEN], vec_mp, double *, char *, char *);
+int     read_shak(char *, mat_mp, char (*)[NLEN], vec_mp, double *, char *, double *);
+int     read_xtl(char *, mat_mp, char (*)[NLEN], vec_mp, double *, char *, char *);
+int     read_xyz(char *, mat_mp, char (*)[NLEN], vec_mp, char *);
+
+/* sginfo functions */
+int     sgexpand(int , int , vec_mt *, char (*)[NLEN], double *, char *);
+
+/* Moldy functions */
+double  det(mat_mt a);
